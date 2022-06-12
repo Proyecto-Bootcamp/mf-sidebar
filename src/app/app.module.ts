bef759671afe25e2fb6fede57b522377010be28c
JuanmiCLucena <jmcostalucena2000@gmail.com>
2022-06-12 15:53:12 +0200

    mf-sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EmptyRouteComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
